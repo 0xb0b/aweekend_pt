1dd4d6d652dec1d46a569ddae303250897ec3999
Bohdan Budnyy <bohdanxbudnyy@gmail.com>
2019-10-14 17:07:31 +0300

    added dielectric material

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <light/Ray.h>
 
 
+const float bias = 0.001f;
 const int maxDepth = 4;
 
 
@@ -41,6 +42,11 @@ struct ScatterInfo
   Color3 albedo;
   Vector3f direction;
 
+  ScatterInfo()
+    : albedo {zero<Color3>}
+    , direction {zero<Vector3f>}
+  {}
+
   ScatterInfo(Color3 a, Vector3f d)
     : albedo {a}
     , direction {d}
@@ -61,7 +67,8 @@ struct Material
   {}
 
   template<typename T>
-  static ScatterInfo invoke(const void* handle, const Vector3f& incident, const Vector3f& normal)
+  static Maybe<ScatterInfo>
+  invoke(const void* handle, const Vector3f& incident, const Vector3f& normal)
   {
     const auto& m = *(static_cast<const T*>(handle));
     return scatter(incident, normal, m);
@@ -69,7 +76,7 @@ struct Material
 
   // type is erased!
   const void* handle;
-  ScatterInfo (*dispatch)(const void*, const Vector3f&, const Vector3f&);
+  Maybe<ScatterInfo> (*dispatch)(const void*, const Vector3f&, const Vector3f&);
 };
 
 struct Lambertian
@@ -83,10 +90,34 @@ struct Lambertian
 
 struct Metallic
 {
+  float fuzzRadius;
   Color3 albedo;
 
-  explicit Metallic(Color3 a)
-    : albedo {a}
+  explicit Metallic(Color3 a, float r = 0.0f)
+    : fuzzRadius {r}
+    , albedo {a}
+  {}
+};
+
+struct Dielectric
+{
+  float n;
+  float nReciprocal;
+  float n2;
+  float nReciprocal2;
+  float cosCritical;
+  float rSchlick;
+  // TODO better name for this? this models how dielectric absorbs the light
+  Color3 albedo;
+
+  explicit Dielectric(float refractionIndex, Color3 a = unit<Color3>)
+    : n {refractionIndex}
+    , nReciprocal {1 / n}
+    , n2 {n * n}
+    , nReciprocal2 {1 / n2}
+    , cosCritical {std::sqrt(1 - nReciprocal2)}
+    , rSchlick {(1 - n) * (1 - n) / ((1 + n) * (1 + n))}
+    , albedo {a}
   {}
 };
 
@@ -162,44 +193,112 @@ Maybe<HitInfo> intersect(const Ray& r, const Sphere& obj)
 {
   const auto centerToRayOrigin {toVector(r.origin - obj.center)};
   const auto a = dot(r.direction, r.direction);
-  const auto b = dot(centerToRayOrigin, r.direction);
+  const auto b = - dot(centerToRayOrigin, r.direction);
   const auto c = dot(centerToRayOrigin, centerToRayOrigin) - obj.radius * obj.radius;
   const auto discriminant = b * b - a * c;
   if (discriminant > 0.0f)
   {
+    const float droot = std::sqrt(discriminant);
     // return closest point
-    const float t = (- b - sqrt(discriminant)) / a;
-    if (t > 0.0f)
+    auto x1 = b - droot;
+    auto x2 = b + droot;
+    auto t = x1;
+    // move slightly forward along the ray (bias) when checking for the intersection point
+    // this is for the intersection point do not end up being on the wrong side of the sphere
+    // due to the numerical error
+    if (x2 > bias)
     {
-      auto hitPoint {translate(r.origin, scale(r.direction, t))};
-      auto normal {toVector(hitPoint - obj.center)};
-      scale_m(normal, 1.0f / obj.radius);
-      // move intersection point slightly so it is not inside the sphere due to numerical error
-      // translate_m(intersectionPoint, scale(normal, 0.001f));
-      return HitInfo(t, hitPoint, normal, obj.material);
+      if (x1 > bias)
+        t /= a;
+      else
+        t = x2 / a;
     }
+    else
+    {
+      return {};
+    }
+    auto hitPoint {translate(r.origin, scale(r.direction, t))};
+    auto normal {toVector(hitPoint - obj.center)};
+    scale_m(normal, 1.0f / obj.radius);
+    return HitInfo(t, hitPoint, normal, obj.material);
   }
   return {};
 }
 
-ScatterInfo scatter(const Vector3f incident, const Vector3f normal, const Material& m)
+Maybe<ScatterInfo>
+scatter(const Vector3f incident, const Vector3f normal, const Material& m)
 {
   return (*m.dispatch)(m.handle, incident, normal);
 }
 
-ScatterInfo scatter(const Vector3f incident, const Vector3f normal, const Lambertian& m)
+Maybe<ScatterInfo>
+scatter(const Vector3f incident, const Vector3f normal, const Lambertian& m)
 {
   auto nextRayDir {normal};
-  // TODO what happens if this is (approximately)zero vector?
   nextRayDir += randomInUnitSphereByRej();
-  return {m.albedo, nextRayDir};
+  if (dot(nextRayDir, normal) > 0.0f)
+    return ScatterInfo(m.albedo, nextRayDir);
+  else
+    return {};
 }
 
-ScatterInfo scatter(const Vector3f incident, const Vector3f normal, const Metallic& m)
+Maybe<ScatterInfo>
+scatter(const Vector3f incident, const Vector3f normal, const Metallic& m)
 {
   auto nextRayDir {incident};
   nextRayDir -= scale(normal, 2 * dot(incident, normal));
-  return {m.albedo, nextRayDir};
+  if (m.fuzzRadius > 0.0f)
+    nextRayDir += scale(randomInUnitSphereByRej(), m.fuzzRadius);
+  if (dot(nextRayDir, normal) > 0.0f)
+    return ScatterInfo(m.albedo, nextRayDir);
+  else
+    return {};
+}
+
+Maybe<ScatterInfo>
+scatter(const Vector3f incident, const Vector3f normal, const Dielectric& m)
+{
+  auto nextRayDir {incident};
+  const auto cosIncident = dot(incident, normal);
+  if (cosIncident <= 0.0f)
+  {
+    // incident into the volume
+    float reflectivity = m.rSchlick + (1 - m.rSchlick) * pow((1 + cosIncident), 5);
+    if (random() < reflectivity)
+      nextRayDir -= scale(normal, 2 * cosIncident);
+    else
+    {
+      auto k = m.nReciprocal * cosIncident;
+      float cosTransmitted = std::sqrt(1 - m.nReciprocal2 + k * k);
+      k += cosTransmitted;
+      scale_m(nextRayDir, m.nReciprocal);
+      nextRayDir -= scale(normal, k);
+    }
+  }
+  else
+  {
+    // incident out of the volume
+    if (cosIncident > m.cosCritical)
+    {
+      auto k = m.n * cosIncident;
+      float cosTransmitted = std::sqrt(1 - m.n2 + k * k);
+      float reflectivity = m.rSchlick + (1 - m.rSchlick) * pow((1 - cosTransmitted), 5);
+      if (random() < reflectivity)
+        nextRayDir -= scale(normal, 2 * cosIncident);
+      else
+      {
+        k -= cosTransmitted;
+        scale_m(nextRayDir, m.n);
+        nextRayDir -= scale(normal, k);
+      }
+    }
+    else
+    {
+      // total internal reflection
+      nextRayDir -= scale(normal, 2 * cosIncident);
+    }
+  }
+  return ScatterInfo(m.albedo, nextRayDir);
 }
 
 Maybe<HitInfo> intersect(const Ray& r, const ObjList& world)
@@ -232,11 +331,18 @@ Color3 trace(const Ray& r, const ObjList& world, int depth=0)
   if (maybeHit)
   {
     const auto& hit = maybeHit.value;
-    const auto interaction {scatter(r.direction, hit.normal, hit.material)};
-    // infinite recursion!
-    const Ray nextRay {hit.point, interaction.direction};
-    auto incoming {trace(nextRay, world, ++depth)};
-    return incoming * interaction.albedo;
+    const auto maybeInteraction {scatter(r.direction, hit.normal, hit.material)};
+    if (maybeInteraction)
+    {
+      const auto& interaction = maybeInteraction.value;
+      const Ray nextRay {hit.point, interaction.direction};
+      auto incoming {trace(nextRay, world, ++depth)};
+      return incoming * interaction.albedo;
+    }
+    else
+    {
+      return zero<Color3>;
+    }
   }
   else
   {
@@ -256,19 +362,25 @@ int main()
 {
   int hresolution = 512;
   int vresolution = 288;
-  const Camera cam {zero<Point3f>, Vector3f(-1.0f, 0.0f, 0.0f), Vector3f(0.0f, 0.0f, 1.0f),
+  const Camera cam {Point3f(1.0f, 0.0f, 0.0f),
+                    Vector3f(-1.0f, 0.0f, 0.0f), Vector3f(0.0f, 0.0f, 1.0f),
                     hresolution, vresolution, Degree(90.0f)};
 
-  const std::vector<Lambertian> diffuseMaterials { Lambertian(Color3(0.7f, 0.3f, 0.3f)),
-                                                   Lambertian(Color3(0.3f, 0.3f, 0.3f)) };
-  const std::vector<Metallic> metalMaterials { Metallic(Color3(0.8f, 0.6f, 0.2f)),
-                                               Metallic(Color3(0.8f, 0.8f, 0.8f)) };
+  const std::array<Lambertian, 2> diffuseMaterials { Lambertian(Color3(0.7f, 0.3f, 0.3f)),
+                                                     Lambertian(Color3(0.3f, 0.3f, 0.3f)) };
+  const std::array<Metallic, 2> metalMaterials { Metallic(Color3(0.8f, 0.6f, 0.2f)),
+                                                 Metallic(Color3(0.8f, 0.8f, 0.8f), 0.3f) };
+  const std::array<Dielectric, 2> dielectricMaterials {
+    Dielectric(1.5f),
+    Dielectric(1.5f, Color3(1.0f, 0.0f, 1.0f)) };
 
   const std::vector<Sphere> spheres {
     {0.5f, Point3f(-2.0f, 0.0f, 0.0f), Material(diffuseMaterials[0])},
     {100.0f, Point3f(-2.0f, 0.0f, -100.5f), Material(diffuseMaterials[1])},
-    {0.8f, Point3f(-2.0f, 1.3f, 0.3f), Material(metalMaterials[0])},
-    {0.4f, Point3f(-2.0f, -0.9f, -0.1f), Material(metalMaterials[1])}
+    // {0.8f, Point3f(-2.0f, 1.3f, 0.3f), Material(metalMaterials[0])},
+    {0.8f, Point3f(-2.0f, 1.3f, 0.3f), Material(dielectricMaterials[0])},
+    // {0.4f, Point3f(-2.0f, -0.9f, -0.1f), Material(metalMaterials[1])}
+    {0.4f, Point3f(-2.0f, -0.9f, -0.1f), Material(dielectricMaterials[1])}
   };
 
   const ObjList world { Procedural(spheres[0]), Procedural(spheres[1]),
